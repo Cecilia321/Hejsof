fb75c52ca4c92ec6edb6eafccd4a43113cfa83b0
SofieAmalie44 <sofieamaliethorlund@gmail.com>
2023-12-04 20:02:37 +0100

    First commit and push

---
+++ main.js
@@ -0,0 +1,45 @@
+const sofAge = 22;
+const cillaAge = 22;
+
+
+const sum = sofAge + cillaAge;
+
+document.write("We are this old: " + sum);
+
+console.log("hello world!")
+
+const greeting = "hello";
+
+console.log(greeting)
+
+console.log(typeof "3"+5);
+
+
+const teachers = 65
+const students = 40
+
+console.log(`percentage of teachers: ${teachers}%`)
+console.log(`percentage of students: ${students}%`)
+
+const number = "3";
+const string = 3;
+const boolean = typeof string === "number" && number == string;
+console.log(boolean);
+
+const balance = 3300
+if(balance > 2000) {
+    console.log("over 2000");
+} else if (balance > 3000) {
+    console.log("over 3000");
+} else if(balance === 3500) {
+    console.log("exactly 3000");
+} else {
+    console.log("Something else");
+}
+
+for(let j = 4; j >= 0; j--) {
+    console.log(j);
+}
+
+
+
